a9c7a710b124734459849d00d4dd28d4f32baa0f
Gilbert Nepomuceno <gilbertnepomuceno99@gmail.com>
2023-09-04 22:10:15 -0700

    added client side nextjs

---
+++ client/pages/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const index = () => {
+  return (
+    <div>
+      <h1 className="text-xl font-bold text-yellow-300">Testing</h1>
+      <p className="text-yellow-200">
+        Testing to make sure everything is working, had trouble with new nextjs
+        app verions
+      </p>
+    </div>
+  );
+};
+
+export default index;
