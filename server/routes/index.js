db8b5ac5af3c3ef2b7973582d123da8e476c0370
Gilbert Nepomuceno <gilbertnepomuceno99@gmail.com>
2023-09-09 22:04:32 -0700

    fixed bug

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,12 +1,13 @@
 import express from "express";
 
-import { createNote, getNotes, createUser } from "../controllers/note.js";
+import { createNote, getNotes } from "../controllers/note.js";
+import { signUp } from "../controllers/user.js";
 
 const router = express.Router();
 
 // POST Routes
 router.post("/createnote", createNote);
-router.post("/signup", createUser);
+router.post("/signup", signUp);
 
 // GET Routes
 router.get("/notes", getNotes);
