2f4a591c8c058680a2ac28203e22a4ce613e544b
Gilbert Nepomuceno <gilbertnepomuceno99@gmail.com>
2023-10-01 20:09:22 -0700

    added deleting controller

--- server/controllers/note.js
+++ server/controllers/note.js
@@ -21,3 +21,12 @@ export const getNotes = async (req, res) => {
     res.status(404).json({ message: error.message });
   }
 };
+
+export const deleteNote = async (req, res) => {
+  const { id: _id } = req.params;
+  if (!mongoose.Types.ObjectId.isValid(_id)) {
+    return res.status(404).send("No post with that id");
+  }
+  await Note.findByIdAndRemove(_id);
+  res.json({ message: "Post deleted successfully" });
+};
